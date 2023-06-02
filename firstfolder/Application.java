69b79f3f0a71bc1367fa8713c864a69317a1abff
dariusz.szopa <dariusz.szopa@fdmgroup.com>
2023-06-02 10:50:37 +0200

    add folder structure

--- firstfolder/Application.java
+++ firstfolder/Application.java
@@ -1,4 +1,4 @@
-public class Runner {
+public class Application {
     public static void main(String[] args) {
         String message = "Hello World";
         System.out.println("first message is: " + message);
